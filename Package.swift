a475cefceb3c587eda375a5c85650d5df7e75d35
Aviad Segev <aviads@checkpoint.com>
2024-07-22 15:15:12 +0300

    Updated package to use our forked swiftNIO

--- Package.swift
+++ Package.swift
@@ -36,7 +36,7 @@ import class Foundation.ProcessInfo
 func generateDependencies() -> [Package.Dependency] {
     if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
         return [
-            .package(url: "https://github.com/apple/swift-nio.git", from: "2.54.0"),
+            .package(url: "https://github.com/chkp-aviads/swift-nio.git", branch: "main"),
             .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
         ]
     } else {
